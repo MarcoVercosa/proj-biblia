cdf710bf3ad23ec6eaeb2a12e25d7dab882034b8
MarcoVercosa <marco2007sky@hotmail.com>
2021-09-03 21:11:37 -0300

    Commit Bible

--- client/src/fetch/api.js
+++ client/src/fetch/api.js
@@ -1,9 +1,12 @@
 import axios from "axios"
 
+let local = "192.168.0.150"
+let publico = "fontedevida.mavs.vps-kinghost.net"
+
 
 export default async function GetAPI(livro, nome) {
 
-    const resultado = await axios.get(`http://191.181.11.42:9000/${livro}`)
+    const resultado = await axios.get(`http://${publico}:9000/${livro}`)
     //console.log(resultado)
     return resultado
 
