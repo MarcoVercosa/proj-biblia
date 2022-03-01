5a2338482009ca1e571b20fef8688b19fe9dc4b8
MarcoVercosa <marco2007sky@hotmail.com>
2022-03-01 01:41:42 -0300

    Remastering code

--- server/build/repositories/curiosidades/curiosidadesRepository.js
+++ server/build/repositories/curiosidades/curiosidadesRepository.js
@@ -5,8 +5,9 @@ const conexao_1 = require("../../infrabd/conexao");
 class CuriosidadesRepository {
     constructor() { }
     BuscaCuriosidade(palavra) {
+        console.log(palavra);
         return new Promise((resolve, reject) => {
-            const sql = `SELECT livro, conteudo FROM biblias13v.curiosidades WHERE livro LIKE "%${palavra}%"`;
+            const sql = `SELECT livro, conteudo FROM biblias13v.curiosidades WHERE livro = "${palavra}"`;
             conexao_1.conectaBD.query(sql, (erro, resultado) => {
                 if (erro) {
                     return reject(erro);
--- server/src/repositories/curiosidades/curiosidadesRepository.ts
+++ server/src/repositories/curiosidades/curiosidadesRepository.ts
@@ -13,8 +13,9 @@ export class CuriosidadesRepository implements ICuriosidadesRepository {
     ) { }
 
     BuscaCuriosidade(palavra: string): Promise<IResultado> {
+        console.log(palavra)
         return new Promise((resolve: any, reject: any) => {
-            const sql = `SELECT livro, conteudo FROM biblias13v.curiosidades WHERE livro LIKE "%${palavra}%"`
+            const sql = `SELECT livro, conteudo FROM biblias13v.curiosidades WHERE livro = "${palavra}"`
             conectaBD.query(sql, (erro: any, resultado: Array<IResultado>) => {
                 if (erro) {
                     return reject(erro)
